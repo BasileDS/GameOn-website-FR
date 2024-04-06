a76741b491e8a97a19a5802e7d92b16b3c842732
BasileDS <basile.d.santos@gmail.com>
2024-04-06 10:10:34 +0200

    Delete JS in the HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,26 +1,39 @@
-function editNav() {
-  let header = document.getElementById("myTopnav");
-  if (header.className === "topnav") {
-    header.className += " responsive";
-  } else {
-    header.className = "topnav";
-  }
-}
-
 // DOM Elements
+const burger = document.querySelector("#burger");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".modalClose");
+const modalForm = document.querySelector("[name='reserve']");
+
+/****  Listener  ****/
+closeBtn.addEventListener("click", closeModal);
+modalForm.addEventListener("submit", validate);
+burger.addEventListener("click", editNav);
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", openModal));
 
-// launch modal form
-function launchModal() {
+// open modal form
+function openModal() {
   modalbg.style.display = "block";
 }
 
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+function validate(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+function editNav() {
+let header = document.getElementById("myTopnav");
+  if (header.className === "topnav") {
+    header.className += " responsive";
+  } else {
+    header.className = "topnav";
+  }
 }
