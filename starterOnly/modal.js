fcac266dbfb9d3806f3fd709fbfb8cd7bb999b59
BasileDS <basile.d.santos@gmail.com>
2024-04-18 19:13:44 +0200

    Final version / all test done + mobile vers checkd

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,40 +1,52 @@
 // DOM Elements
 const burger = document.querySelector("#burger");
 const modalbg = document.querySelector(".bground");
+const modalCtnt = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".modalClose");
 const modalForm = document.querySelector("[name='reserve']");
 const formInputs = document.querySelectorAll("[name='reserve'] input");
+const dateInput = document.querySelector("#birthdate");
 const successDiv = document.querySelector('.formSuccess');
 const newRegistrationButton = document.querySelector('#newRegBtn');
-
+const formData = document.querySelectorAll('.formData');
 
 /****  Listeners  ****/
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", openModal));
 closeBtn.addEventListener("click", closeModal);
 burger.addEventListener("click", editNav);
+
+// Wait modal animation end to hide modal
+modalCtnt.addEventListener("animationend", () => {
+  const isModalClosed = modalCtnt.classList.contains('content-close');
+  if (isModalClosed) {
+    modalCtnt.classList.remove('content-close');
+    modalbg.style.display = "none";
+  }
+});
+
+// Switch success message to a reseted Form if new form button is clicked
+newRegistrationButton.addEventListener("click", (event) => {
+  modalForm.reset();
+  removeFormAttributes();
+  modalForm.classList.toggle('hidden');
+  successDiv.classList.toggle('hidden');
+});
+
+// Check all input values and show success message if passed
 modalForm.addEventListener("submit", (event) => {
   event.preventDefault();
   formValidation();
 });
-newRegistrationButton.addEventListener("click", (event) => {
-  modalForm.reset();
-  modalForm.classList.remove('hidden');
-  successDiv.classList.add('hidden');
-})
 
-
-
-
-
-// Listen to the fields before submiting
-/*modalForm.addEventListener('change', (event) => {
-  const valeurNom = event.target.value;
-  if (valeurNom === "bbb") {
-      console.log('Le champ nom est vide');
-  } else {
-      console.log('Le champ nom est rempli');
-  }
-});*/
+// Stores inputs in an array
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("change", (event) => {
+    event.preventDefault();
+    verifyInput(formInputs[i]);
+  });
+}
 
 // Form validation
 function formValidation() {
@@ -47,26 +59,17 @@ function formValidation() {
       successDiv.classList.remove('hidden');
     }
   }
-
+  // loop on inputs to check and submit if clean
   for (let i = 0; i < formInputs.length; i++) {
     const inputField = formInputs[i];
     inputField.value = inputField.value.trim();
     switch (inputField.id) {
       case 'first':
       case 'last':
-        if (checkLenght(inputField, 2) === false) {
-          inputField.closest('.formData').setAttribute('data-error-visible', 'true');
-          inputField.closest('.formData').setAttribute('data-error', 'Le champ doit contenir au moins 2 caractères');
-        } else if (checkCaracters(inputField) === false) {
-          inputField.closest('.formData').setAttribute('data-error-visible', 'true');
-          inputField.closest('.formData').setAttribute('data-error', 'Le champ doit contenir uniquement des lettres');
-        } else {
-          inputField.closest('.formData').setAttribute('data-error-visible', 'false');
-          successScore++;
-        }
+        !isEmpty(inputField) ? !checkLenght(inputField, 2) ? !checkCaracters(inputField) ? successScore++ : '' : '' : '';
         break;
       case 'email':
-        validateEmail(inputField) ? successScore++ : '';
+        !isEmpty(inputField) ? validateEmail(inputField) ? successScore++ : '' : '';
         break;
       case 'birthdate':
         !isEmpty(inputField) ? !validateDate(inputField) ? successScore++ : '' : '';
@@ -81,8 +84,6 @@ function formValidation() {
         break;
     }
     //check if a location has been selected
-
-    console.log(successScore);
     showSuccessMessage();
   }
 }
@@ -110,12 +111,16 @@ function isEmpty(e) {
 
 // check if 'e'lement length is higher than 'n'umber
 function checkLenght(e, n) {
-  if (e.textLength < n) {
+  inputValue = e.value;
+  inputValueLength = inputValue.length;
+  console.log(e);
+  if (inputValueLength < n) {
     e.closest('.formData').setAttribute('data-error-visible', 'true');
     e.closest('.formData').setAttribute('data-error', 'Le champ doit contenir au moins 2 caractères');
-    return false;
+    return true;
   } else {
     e.closest('.formData').setAttribute('data-error-visible', 'false');
+    return false;
   }
 }
 
@@ -124,6 +129,8 @@ function checkCaracters(e) {
   const regex = /^[a-zA-Z]+(?: [a-zA-Z]+)*$/;
   const result = regex.test(e.value);
   if (!result) {
+    e.closest('.formData').setAttribute('data-error-visible', 'true');
+    e.closest('.formData').setAttribute('data-error', 'Le champ doit contenir uniquement des lettres');
     return false;
   } else {
     e.closest('.formData').setAttribute('data-error-visible', 'false');
@@ -143,11 +150,26 @@ function validateEmail(e) {
   }
 }
 
+// compare current date and input date
+function isPastDate(e) {
+  const inputDate = e.valueAsDate;
+  inputDateTime = inputDate.getTime();
+  const date = new Date();
+  const currentDateTime = date.getTime();
+  if (inputDateTime < currentDateTime) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 // check if the birthdate format is valid
 function validateDate(e) {
   const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
   const result = regex.test(e.value);
-  if (!result) {
+  const dateTest = isPastDate(e);
+  if (!result || !dateTest) {
     e.closest('.formData').setAttribute('data-error-visible', 'true');
     e.closest('.formData').setAttribute('data-error', 'Veuillez saisir une date valide');
   } else {
@@ -161,7 +183,7 @@ function checkQuantity(e, f, l) {
     e.closest('.formData').setAttribute('data-error-visible', 'false');
   } else {
     e.closest('.formData').setAttribute('data-error-visible', 'true');
-    e.closest('.formData').setAttribute('data-error', 'Le champ doit contenir un chiffre entre 0 et 100 svp');
+    e.closest('.formData').setAttribute('data-error', 'Veuillez sélectionner un chiffre entre 0 et 99');
   }
 }
 
@@ -175,6 +197,38 @@ function isTermAndConditionsChecked(e) {
   } 
 }
 
+// Remove all formData attributes
+function removeFormAttributes() {
+  for (let i = 0; i < formData.length; i++) {
+    const formDataInput = formData[i];
+    formDataInput.removeAttribute('data-error-visible');  
+  }
+}
+
+// Check input according to the id
+function verifyInput(e) {
+  switch (e.id) {
+    case 'first':
+    case 'last':
+      !isEmpty(e) ? !checkLenght(e, 2) ? !checkCaracters(e) ? '' : '' : '' : '';
+      break;
+    case 'email':
+      !isEmpty(e) ? validateEmail(e) ? '' : '' : '';
+      break;
+    case 'birthdate':
+      !isEmpty(e) ? !validateDate(e) ? '' : '' : '';
+      break;
+    case 'quantity':
+      !isEmpty(e) ? !checkQuantity(e, 0, 100) ? '' : '' : '';
+      break;
+    case 'checkbox1':
+      isTermAndConditionsChecked(e) ? '' : '';
+      break;
+    default:
+      break;
+  }
+}
+
 // Header responsive elements
 function editNav() {
   let header = document.getElementById("myTopnav");
@@ -185,9 +239,6 @@ function editNav() {
     }
 }
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", openModal));
-
 // open modal form
 function openModal() {
   modalbg.style.display = "block";
@@ -195,5 +246,5 @@ function openModal() {
 
 // Close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+  modalCtnt.classList.add('content-close');
 }
